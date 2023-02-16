e4edcbe3911bab9e07fcd5f6a7e778d102af3832
konditi1 <konditi1@gmail.com>
2023-02-16 11:42:15 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 *main - passes the sizeof
 *Return: 0 if successfull
 */
-int main(void) 
+int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
