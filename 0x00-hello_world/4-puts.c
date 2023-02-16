cc226d8ad378e7b86f4c0e5ad25dd394415b8ca6
konditi1 <konditi1@gmail.com>
2023-02-16 10:55:07 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main-used to print the statement
- *return:set to 0 to show succes
+ *main - used to print the statement
+ *Return: set to 0 to show succes
  */
 int main(void)
 {
