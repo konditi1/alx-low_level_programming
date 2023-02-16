8034b6c3a1844610eda460ff22f4638da40d98f6
konditi1 <konditi1@gmail.com>
2023-02-16 10:36:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * the main is used to print the statement
+ * the return 0 to show succes
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
