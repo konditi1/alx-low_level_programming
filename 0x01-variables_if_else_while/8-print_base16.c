aed8027b5f1e936bcceb8f00a5615567d1a99122
konditi1 <konditi1@gmail.com>
2023-02-17 15:26:26 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 /**
- * main- entry point
- * Return: 0 is success
- */
-int main() {
-    int i;
+* main- entry point
+* Return: 0 is success
+*/
+int main(void)
+{
+int i;
 
-    for (i = 0; i < 10; i++) {
-        putchar('0' + i);
-    }
+for (i = 0; i < 10; i++)
+putchar('0' + i);
+for (i = 0; i < 6; i++)
+putchar('a' + i);
 
-    for (i = 0; i < 6; i++) {
-        putchar('a' + i);
-    }
 
-    putchar('\n');
-    
-    return (0);
+putchar('\n');
+
+return (0);
 }
 
