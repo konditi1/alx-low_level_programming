304dee041a644db12ca1a89b2bd435bcdd904c7a
konditi1 <konditi1@gmail.com>
2023-02-17 14:58:10 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main- entry point 
+* Return: 0 if successfull
+*/
+int main(void)
+{
+for (int i = 122; i >= 97; i--) {
+putchar(i);
+}
+
+putchar('\n');
+
+return (0);
+}
+
