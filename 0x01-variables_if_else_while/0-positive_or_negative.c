eb044dbcd495b82b976b5abae52d39ae20d7d564
konditi1 <konditi1@gmail.com>
2023-02-17 06:22:39 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,13 +21,12 @@ int main(void)
         }
         else if (n < 0)
         {
-                printf("%d is negative\n", n);
+                 printf("%d is negative\n", n);
         }
         else
         {
                 printf("%d is zero\n", n);
         }
-
         return (0);
 }
 
