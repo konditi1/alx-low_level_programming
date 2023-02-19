6602d21abddc2fe43cd55d8055dc886865843aa6
konditi1 <konditi1@gmail.com>
2023-02-19 05:16:44 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,12 @@
 */
 int main(void)
 {
-int i, j;
+int i;
 
 for (i = 0; i < 10; i++)
 {
-for (j = i + 1; j < 10; j++)
-{
 putchar(i + '0');
-putchar(j + '0');
-putchar((i < 8 || j < 9) ? ',' : '\n');
-}
+putchar((i < 9) ? ',' : '\n');
 }
 
 return (0);
