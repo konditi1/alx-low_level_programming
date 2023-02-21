8b6d05472dd888f80f42d40dc4735952e5fb99cc
konditi1 <konditi1@gmail.com>
2023-02-21 09:00:45 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main- entry point
+* Return:0 if successfull
+*/
+int main(void)
+{
+char *putch_ar = "putchar";
+int i;
+
+if (i = 0; putch_ar[i] != '\0'; i++)
+{
+_putchar(putch_ar[i]);
+_putchar('\n');
+}
+return (0);
+}
