fd52ec4944bedc76d79401044ff39ab4d9d005d6
konditi1 <konditi1@gmail.com>
2023-02-21 11:35:22 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+int _putchar(char a);
+void print_alphabet(void);
+void print_alphabet(void);
+int _islower(int a);
+int _isalpha(int a);
+int print_sign(int b);
+int abs(int);
+int print_digit(int);
