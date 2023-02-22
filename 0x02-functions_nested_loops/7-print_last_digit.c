17e34c9b87c1abb887c16e3d9dddbf0fac9ee66c
konditi1 <konditi1@gmail.com>
2023-02-21 17:07:19 -0800

    7-last_digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,18 @@
 
 /**
 * print_last_digit - prints the last digit of a number.
-* @n:the last digit.
-* Return: the last digit
+* @n: the number to extract the last digit from.
+*
+* Return: the value of the last digit.
 */
-
 int print_last_digit(int n)
 {
-int r = n % 10;
-_putchar(r + '0');
-return (r);
+int last_digit = n % 10;
+
+if (last_digit < 0) /* Handle negative input */
+last_digit = -last_digit;
+
+_putchar(last_digit + '0');
+
+return (last_digit);
 }
