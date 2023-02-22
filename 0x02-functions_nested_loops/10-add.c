eb9214970a8319319277b89f60a2c7857d9e2817
konditi1 <konditi1@gmail.com>
2023-02-22 06:36:41 -0800

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @a: an intiger to be added
+ * @b: an intiger to be added
+ * Return: the sum of the intigers
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
