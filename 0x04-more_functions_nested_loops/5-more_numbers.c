00ef77a6d2a418a33ff27b9a21c05ff4e6ad363e
konditi1 <konditi1@gmail.com>
2023-02-23 14:41:00 -0800

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* more_numbers- prints 10 times the numbers, from 0 to 14
+*/
+
+void more_numbers(void)
+{
+int i, j;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 15; i++)
+{
+if (i < 10)
+{
+_putchar(i + '0');
+
+}
+else
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
