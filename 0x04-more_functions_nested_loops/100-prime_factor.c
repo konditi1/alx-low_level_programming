43ca3af49ba03d8155fdfc2274cf7abb7d9bb761
konditi1 <konditi1@gmail.com>
2023-02-24 12:50:54 -0800

    100 prime numders

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+/**
+ * main- entry point
+ * Return: 0 successful
+ */
 
 int main(void)
 {
 long int num = 612852475143;
 int i;
 
-while (num % 2 == 0) {
+while (num % 2 == 0)
+{
 num /= 2;
 }
 
-for (i = 3; i <= sqrt(num); i += 2) {
-while (num % i == 0) {
+for (i = 3; i <= sqrt(num); i += 2)
+{
+while (num % i == 0)
+{
 num /= i;
 }
 }
 
-if (num > 2) {
+if (num > 2)
+{
 printf("%ld\n", num);
 }
 
-return 0;
+return (0);
 }
 
