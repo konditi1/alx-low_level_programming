6c14d96591355309bb070f9d53e2b280edaef438
konditi1 <konditi1@gmail.com>
2023-02-27 09:06:47 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * reset_to_98- pointer to an int as parameter and update
+ * @n: pointer argument
+ * Return: 0 successfull
+ */
+
+void reset_to_98(int *n);
+{
+n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!bin/bash
+readme for pointers and arrays_string
