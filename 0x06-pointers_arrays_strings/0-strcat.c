4d6eababa58a95788a1326aacb874df8e0c6da1b
konditi1 <konditi1@gmail.com>
2023-03-01 03:03:14 -0800

    first comit for o.06 pointers string and arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _strcat-function that concatenates two strings.
+ * @dest: char data type
+ * @src: char data type
+ * Returns: a pointer to the resulting string dest
+ */
+
+
