0ef635d081b4f7247d5b0053a3c848dae8b5c705
konditi1 <konditi1@gmail.com>
2023-03-06 23:51:42 -0800

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+/**
+* print_times_table - prints the n times table, starting with 0
+* @n: the highest value in the times table
+*/
+void print_times_table(int n)
+{
+if (n < 0 || n > 15)
+return;
+
+for (int i = 0; i <= n; i++)
+{
+printf("%4d", i * j);
+}
+printf("\n");
+}
+}
+
