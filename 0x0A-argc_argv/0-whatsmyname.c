5f9b100cd7bbbe11de892029ccbf20f18010b408
konditi1 <konditi1@gmail.com>
2023-03-10 15:58:17 -0800

    prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+
 /**
 * main - prints the name of the program
-* @argc: count
-* @argv:vector
+*
 * Return: Always 0
 */
-
-int main(int argc, char *argv[])
+int main(void)
 {
-printf("%s\n", argv[0]);
+printf("%s\n", __FILE__);
 return (0);
 }
 
