2424c4005c54193bfefa536459e1867eb8892a03
konditi1 <konditi1@gmail.com>
2023-03-10 14:01:05 -0800

    Create the static library libmy.a

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _islower -checks for lowercase character
+* Return: 1 if c is lowercase and 0 otherwise
+* @c : passed as parameter
+*/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _abs -  computes the absolute value of an integer.
+* @r: any positive or negative parameter
+* Return: an int if succesful
+*/
+
+int _abs(int r)
+{
+if (r < 0)
+{
+return (-r);
+}
+else
+return (r);
+}
