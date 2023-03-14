7d92004059b9f05062f1c93347a7a76f95d63e8a
konditi1 <konditi1@gmail.com>
2023-03-14 07:33:49 -0700

    word seperater

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <string.h>
+#define DELIMITER " "
+
+/**
+* count_words - count the number of words in a string
+* @str: string to count words in
+*
+* Return: number of words in the string
+*/
+static size_t count_words(const char *str)
+{
+size_t count = 0;
+int in_word = 0;
+while (*str)
+{
+if (*str == ' ')
+{
+in_word = 0;
+}
+else if (!in_word)
+{
+in_word = 1;
+count++;
+}
+str++;
+}
+return (count);
+}
+/**
+* strtow - split a string into words
+* @str: string to split
+*
+* Return: pointer to array of words, or NULL if str is NULL or empty
+*/
+char **strtow(char *str)
+{
+size_t i, j, num_words, word_len;
+char **words, *word_start, *word_end;
+
+if (!str || !*str)
+return (NULL);
+num_words = count_words(str);
+words = malloc(sizeof(char *) * (num_words + 1));
+if (!words)
+return (NULL);
+i = 0;
+while (*str)
+{
+if (*str == ' ')
+{
+str++;
+continue;
+}
+word_start = str;
+word_len = 0;
+while (*str && *str != ' ')
+{
+word_len++;
+str++;
+}
+word_end = str - 1;
+words[i] = malloc(sizeof(char) * (word_len + 1));
+if (!words[i])
+{
+while (i--)
+free(words[i]);
+free(words);
+return (NULL);
+}
+for (j = 0; j < word_len; j++)
+words[i][j] = word_start[j];
+words[i][j] = '\0';
+i++;
+}
+words[i] = NULL;
+return (words);
+}
+
