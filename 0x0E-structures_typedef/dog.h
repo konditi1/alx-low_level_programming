4e3a4e7a351301d6c1f6609284b6b754770c7abc
konditi1 <konditi1@gmail.com>
2023-03-20 08:03:53 -0700

    Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+/**
+ * struct dog - Short description
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* MYHEADER_H */
+
