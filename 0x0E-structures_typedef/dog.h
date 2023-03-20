57c7277608bf51713fd55ec5a3201aeeb9b67de7
konditi1 <konditi1@gmail.com>
2023-03-20 12:52:40 -0700

    a new type dog_t as a new name for the type struct dog.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,8 +12,9 @@ struct dog
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _putchar(char c);
 #endif /* MYHEADER_H */
 
