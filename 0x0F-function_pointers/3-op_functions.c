5bd4a6ba580fe9923853333168582104760b2ad9
konditi1 <konditi1@gmail.com>
2023-03-22 12:54:55 -0700

    calculator app

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - returns the product of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - returns the division result of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: division result of a and b
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - returns the modulo of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: modulo of a and b
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
