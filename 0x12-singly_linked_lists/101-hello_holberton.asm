5bc7d7618c1e961588fd55fd294775bd1acd07ad
konditi1 <konditi1@gmail.com>
2023-03-30 13:10:16 -0700

    a 64-bit program in assembly that prints

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,20 +5,20 @@ fmt: db '%s', 10, 0
 section .text
 global _start
 
-_start:
+main:
 ; Push arguments to stack
 push qword fmt
 push qword hello
 
 ; Call printf
 mov rax, 0x000000000000003B ; System call number for `execve`
-mov rdi, qword rsp ; Address of format string
-xor rsi, rsi ; No arguments passed
-xor rdx, rdx ; No environment variables passed
-syscall ; Call printf
+mov rdi, qword rsp         ; Address of format string
+xor rsi, rsi              ; No arguments passed
+xor rdx, rdx              ; No environment variables passed
+syscall                   ; Call printf
 
 ; Exit program
 mov rax, 0x000000000000003C ; System call number for `exit`
-xor rdi, rdi ; Return code 0
-syscall ; Call exit
+xor rdi, rdi              ; Return code 0
+syscall                   ; Call exit
 
