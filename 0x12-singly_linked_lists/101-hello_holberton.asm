390e99d57fd7d6909d760161a14a79ad4e8efd25
konditi1 <konditi1@gmail.com>
2023-03-30 12:31:02 -0700

    asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,24 @@
+section .data
+hello: db 'Hello, Holberton', 10, 0
+fmt: db '%s', 10, 0
+
+section .text
+global _start
+
+_start:
+; Push arguments to stack
+push qword fmt
+push qword hello
+
+; Call printf
+mov rax, 0x000000000000003B ; System call number for `execve`
+mov rdi, qword rsp ; Address of format string
+xor rsi, rsi ; No arguments passed
+xor rdx, rdx ; No environment variables passed
+syscall ; Call printf
+
+; Exit program
+mov rax, 0x000000000000003C ; System call number for `exit`
+xor rdi, rdi ; Return code 0
+syscall ; Call exit
+
