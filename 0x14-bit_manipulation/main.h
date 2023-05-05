36578653014ae55b99a5e7e56d3bbc9efcf1dc66
konditi1 <konditi1@gmail.com>
2023-05-05 10:20:58 -0700

    betty for main.h and created 101 password

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
- 
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
