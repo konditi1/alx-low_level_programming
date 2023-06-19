536a88b9bae2653ea28ee5673d12314dfc2048b8
konditi1 <konditi1@gmail.com>
2023-06-19 11:09:20 -0700

    a dynamic library that contains C functions that can be called from Python

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "void srand(unsigned int seed) {}" > fake_rand.c
-echo "int rand(void) { return $(( $1 )); }" >> fake_rand.c
-gcc -shared -o fake_rand.so -fPIC fake_rand.c
-
-export LD_PRELOAD=./fake_rand.so
