42de9204ebd0b246ca2a6f0f35d3a6220e68bd24
konditi1 <konditi1@gmail.com>
2023-08-24 02:43:34 +0300

    earea of an island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in grid.
+
+    Args:
+    - grid (list of list of integers): The grid describing the island.
+
+    Returns:
+    - int: The perimeter of the island.
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4  # Initialize with the perimeter of a square
+
+                # Check neighbors
+                if i > 0 and grid[i - 1][j] == 1:  # Check above
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:  # Check left
+                    perimeter -= 2
+
+    return perimeter
